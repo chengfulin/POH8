eb15225a1a964f974a9681116b966107d49a5bd9
Jerry Lin <jerry_lin@maxense.com.tw>
2016-06-10 19:59:15 +0800

    Solution for hastukoi_hair3
    Solution for hastukoi_hair3

---
+++ hatsukoi_hair3.js
@@ -0,0 +1,21 @@
+var readline = require("readline");
+var rl = readline.createInterface(process.stdin, process.stdout);
+
+var inputs = [];
+rl.on("line", function (line) {
+	inputs.push(line.trim());
+}).on("close", function () {
+	start();
+})
+
+var indexOfInputs = {
+	"chcekingNumber": 0
+};
+
+function start() {
+	var num = parseInt(inputs[indexOfInputs.chcekingNumber]);
+	if (num % 7 === 0)
+		console.log("lucky");
+	else
+		console.log("unlucky");
+}
