98676d470d76f4de6ad3d84468b30ac4cf36f284
Jerry Lin <jerry_lin@maxense.com.tw>
2016-06-10 19:55:39 +0800

    Solution for hastukoi_hair2
    Solution for hastukoi_hair2

---
+++ hatsukoi_hair2.js
@@ -0,0 +1,21 @@
+var readline = require("readline");
+var rl = readline.createInterface(process.stdin, process.stdout);
+
+var inputs = [];
+rl.on("line", function (line) {
+	inputs.push(line.trim());
+}).on("close", function () {
+	start();
+})
+
+var indexOfInputs = {
+	"numOfOutputs": 0,
+	"outputString": 1
+};
+
+function start() {
+	var numOfOutputs = parseInt(inputs[indexOfInputs.numOfOutputs]);
+	for (var index = 0; index < numOfOutputs; ++index) {
+		console.log(inputs[indexOfInputs.outputString]);
+	}
+}
